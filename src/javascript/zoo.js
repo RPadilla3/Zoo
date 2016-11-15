a793675038dcb93c2c22ce565adae83ffb9e4d34
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-15 15:58:19 -0500

    Complete redesign, changing the way I'm handling the situation.

--- src/javascript/zoo.js
+++ src/javascript/zoo.js
@@ -6,25 +6,37 @@
      * Animal contructor function that will create a new animal when given a
      * new name and date of birth.
      * @param {String} name Whatever you want to name the animal
-     * @param {Number} DOB A number that will be given to the animal for its age
+     * @param {Number} age A number that will be given to the animal for its age
      * in years.
      */
-    function Animal(name, DOB) {
+    function Animal(name, age) {
         this.name = name || 'joey';
-        this.DOB = DOB || 3;
+        this.age = age || 3;
+
     }
 
-    Animal.prototype.getAge = function getAge(){
-      return 5;
+    // Animal.prototype = Object.create(Narwhal.prototype);
+    // Animal.prototype.constructor = Animal;
 
+    /**
+     * Narhwal constructor function, to be applied to the animal object.
+     * @param {String} name A string, the name of the Narwhal
+     * @param {Number} age  The number that is given to the Narwhal as its age. 
+     */
+    function Narwhal(name, age) {
+
+      Animal.apply( this, [name])
+      this.breed = 'Narwhal';
+      this.name = name || 'Harry Johnson';
+
+    }
+
+    Narwhal.prototype = Object.create(Animal.prototype);
+    Narwhal.prototype.constructor = Narwhal;
+
+    Narwhal.prototype.mate = function mate() {
+      console.log('mating narwhals');
     };
 
-      var Narwhal = new Animal('guadalupe', 3346372934730)
-
-      console.log(Narwhal, Narwhal.getAge());
-
-      var Okapi = new Animal('esperanza', 56828364239462)
-
-      console.log(Okapi, Okapi.getAge());
 
 }());
