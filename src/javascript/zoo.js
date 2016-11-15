61c431a6dbe80e7cc94fb177a8b35c28d6836add
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-15 14:20:50 -0500

    Created a function that with input will return an animal with a name and with a date of birth.

--- src/javascript/zoo.js
+++ src/javascript/zoo.js
@@ -1,4 +1,20 @@
 (function() {
-  'use strict';
-    console.log('connected');
+    'use strict';
+
+    function Animal(name, DOB) {
+        console.log('inside Animal');
+        this.name = name || 'joey';
+        this.DOB = DOB || 3;
+    }
+    console.log(new Animal('guadalupe', 10));
+
+    function Narwhal(name, sex, birth) {
+
+    }
+    Narwhal();
+
+
+    var guadalupe = new Narwhal('guadalupe', 'male', false);
+    var maria = new Narwhal('maria', 'female', true);
+    console.log('guadalupe: ', guadalupe);
 }());
