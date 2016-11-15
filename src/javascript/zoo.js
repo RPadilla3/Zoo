e1e7c8227097096c2383e84009b17bda71321a82
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-15 14:30:42 -0500

    Added Doc Blocker to the Animal constructor function.

--- src/javascript/zoo.js
+++ src/javascript/zoo.js
@@ -1,20 +1,24 @@
 (function() {
     'use strict';
 
+
+    /**
+     * Animal contructor function that will create a new animal when given a
+     * new name and date of birth.
+     * @param {String} name Whatever you want to name the animal
+     * @param {Number} DOB A number that will be given to the animal for its age
+     * in years.
+     */
     function Animal(name, DOB) {
-        console.log('inside Animal');
         this.name = name || 'joey';
         this.DOB = DOB || 3;
     }
     console.log(new Animal('guadalupe', 10));
 
-    function Narwhal(name, sex, birth) {
-
-    }
-    Narwhal();
+  
 
 
-    var guadalupe = new Narwhal('guadalupe', 'male', false);
-    var maria = new Narwhal('maria', 'female', true);
-    console.log('guadalupe: ', guadalupe);
+
+    // var guadalupe = new Narwhal('guadalupe', 'male', false);
+    // var maria = new Narwhal('maria', 'female', true);
 }());
