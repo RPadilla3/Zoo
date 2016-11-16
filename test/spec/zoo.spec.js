712a2915da3db5f23226600037657dbab81c7475
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-16 16:07:26 -0500

    created all tests, including one try & catch method for each.

--- test/spec/zoo.spec.js
+++ test/spec/zoo.spec.js
@@ -4,20 +4,20 @@
 
     var expect = chai.expect;
 
-    describe('Testing namespace of Zoo', function() {
-        it('Should confirm that window.zoo is a namespace', function() {
+    describe('Testing namespace of Zoo.', function() {
+        it('Should confirm that window.zoo is a namespace.', function() {
             expect(window.zoo).to.be.a('object');
         });
     });
 
-    describe('Testing Animal constructor function', function() {
-        it('Should create a new animal whenever a name and age are passed', function() {
+    describe('Testing Animal constructor function.', function() {
+        it('Should create a new animal whenever a name and age are passed.', function() {
             expect(new window.zoo.Animal('poncho', 4)).to.be.an('object');
         })
-        it('Should create the default animal if no input is given', function() {
+        it('Should create the default animal if no input is given.', function() {
             expect(new window.zoo.Animal('')).to.be.an('object');
         })
-        it('Should return a TypeError message if anything other than a string is typed in the input', function() {
+        it('Should return a TypeError message if anything other than a string is typed in the input.', function() {
             // var fn = function(){
             //   new window.zoo.Animal(4);
             // }
@@ -33,14 +33,14 @@
 
     });
 
-    describe('Testing Narwhal Constructor function', function() {
-      it('Should create a new Narwhal whenever a name and age are passed', function() {
+    describe('Testing Narwhal Constructor function.', function() {
+      it('Should create a new Narwhal whenever a name and age are passed.', function() {
           expect(new window.zoo.Narwhal('Jill Stein', 53)).to.be.an('object');
       })
-      it('Should create the default animal if no input is given', function() {
+      it('Should create the default animal if no input is given.', function() {
           expect(new window.zoo.Narwhal('')).to.be.an('object');
       })
-      it('Should return a TypeError message if anythin other than a string is typed in the input', function() {
+      it('Should return a TypeError message if anything other than a string is typed in the input.', function() {
         try {
           new window.zoo.Narwhal(['243']);
           expect(true).to.equal(false);
@@ -49,7 +49,24 @@
         }
       });
 
-  });
+    });
+
+    describe('Testing Echidna Constructor function.', function() {
+      it('Should create a new Echidna whenever the function is called.', function() {
+        expect(new window.zoo.Echidna('Gary Johnson', 48)).to.be.an('object');
+      })
+      it('Should create a default Echidna if no input is given into the arguments.', function() {
+        expect(new window.zoo.Echidna('')).to.be.an('object');
+      })
+      it('Should return a TypeError message if anythinf other than a string is typed in the input.', function() {
+        try {
+          new window.zoo.Echidna(44);
+          expect(true).to.equal(false);
+        } catch(err) {
+          expect(err).to.be.an.instanceof(TypeError);
+        }
+      });
+    });
 
 
 
