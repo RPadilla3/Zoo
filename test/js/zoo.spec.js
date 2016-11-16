f885aae61389b4a5bb4c40f85e132bff57c588bf
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-16 13:07:21 -0500

    all files are properly linked in my test harness, and the tests are up an running.

--- test/js/zoo.spec.js
+++ test/js/zoo.spec.js
@@ -1,4 +1,13 @@
 (function() {
   'use strict';
-  
+  window.zoo = window.zoo || {};
+
+  var expect = chai.expect;
+
+  describe('testing functions of Zoo', function (){
+    it('should confirm that window.zoo is a namespace', function (){
+      expect(window.zoo).to.be.a('object');
+    });
+  });
+
 }());
