58175da93ffa6110bb9cb4311f39ef3fbb453376
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-16 15:34:42 -0500

    Updated indentation.

--- test/spec/zoo.spec.js
+++ test/spec/zoo.spec.js
@@ -1,42 +1,44 @@
 (function() {
-  'use strict';
-  window.zoo = window.zoo || {};
+    'use strict';
+    window.zoo = window.zoo || {};
 
-  var expect = chai.expect;
+    var expect = chai.expect;
 
-  describe('Testing namespace of Zoo', function (){
-    it('Should confirm that window.zoo is a namespace', function (){
-      expect(window.zoo).to.be.a('object');
-    });
-  });
-
-  describe('Testing Animal constructor function', function (){
-    it('Should create a new animal whenever a name and age are passed', function(){
-      expect(new window.zoo.Animal('poncho', 4)).to.be.an('object');
-    })
-    it('Should create the default animal if no input is given', function(){
-      expect(new window.zoo.Animal('')).to.be.an('object');
-    })
-    it('Should return a TypeError message if anything other than a string is typed in the input', function(){
-      // var fn = function(){
-      //   new window.zoo.Animal(4);
-      // }
-      //   // expect(fn).to.throw(Error);
-      try {
-        new window.zoo.Animal(420);
-        expect(true).to.equal(false);
-      } catch(err){
-        console.log(err);
-        expect(err).to.be.an.instanceof(TypeError);
-      }
-
+    describe('Testing namespace of Zoo', function() {
+        it('Should confirm that window.zoo is a namespace', function() {
+            expect(window.zoo).to.be.a('object');
+        });
     });
 
-  });
+    describe('Testing Animal constructor function', function() {
+        it('Should create a new animal whenever a name and age are passed', function() {
+            expect(new window.zoo.Animal('poncho', 4)).to.be.an('object');
+        })
+        it('Should create the default animal if no input is given', function() {
+            expect(new window.zoo.Animal('')).to.be.an('object');
+        })
+        it('Should return a TypeError message if anything other than a string is typed in the input', function() {
+            // var fn = function(){
+            //   new window.zoo.Animal(4);
+            // }
+            //   // expect(fn).to.throw(Error);
+            try {
+                new window.zoo.Animal(420);
+                expect(true).to.equal(false);
+            } catch (err) {
+                expect(err).to.be.an.instanceof(TypeError);
+            }
 
-  describe('Testing Narwhal Constructor function', function(){
-    it('Should create a new Narwhal when ')
-  });
+        });
+
+    });
+
+    // describe('Testing Narwhal Constructor function', function() {
+    //     it('Should create a new Narwhal when', function() {
+    //
+    //     });
+    //
+    // });
 
 
 
