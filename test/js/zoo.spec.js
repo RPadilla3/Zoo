69a5a4b1caeb85d0dfb800447b8a670e86c51c28
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-16 13:21:03 -0500

    Got the second test up and running.

--- test/js/zoo.spec.js
+++ test/js/zoo.spec.js
@@ -4,9 +4,15 @@
 
   var expect = chai.expect;
 
-  describe('testing functions of Zoo', function (){
+  describe('Testing namespace of Zoo', function (){
     it('should confirm that window.zoo is a namespace', function (){
       expect(window.zoo).to.be.a('object');
+    });
+  });
+
+  describe('testing Animal constructor function', function(){
+    it('should create a new animal whenever a name and age are passed', function(){
+      expect(new window.zoo.Animal('poncho', 4)).to.be.an('object');
     });
   });
 
